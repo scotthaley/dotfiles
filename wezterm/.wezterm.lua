602522ebd973c3f546f1e848f59b497d3b0fa91e
Scott Haley <williamscotthaley@gmail.com>
2024-09-16 17:56:47 -0500

    add padding in wezterm for retina screens

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.window_padding = {
-	top = 35,
+	top = 45,
 }
 config.hide_tab_bar_if_only_one_tab = true
 
