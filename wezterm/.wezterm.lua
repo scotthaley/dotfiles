66b63e408cae89f8a757b130065a62e7c4df11a0
Scott Haley <williamscotthaley@gmail.com>
2024-10-29 23:19:26 -0500

    wezterm font changes

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -9,7 +9,8 @@ config.window_padding = {
 config.hide_tab_bar_if_only_one_tab = true
 
 config.color_scheme = "Catppuccin Mocha"
-config.font_size = 15.0
+config.font_size = 16.0
+config.line_height = 1.05
 
 wezterm.on("update-status", function(window, pane)
 	local name = window:active_key_table()
